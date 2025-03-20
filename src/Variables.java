2b6b2e18f40d34b09338bef5fecc77c2c0515edc
tyagi <tyagiishit91@gmail.com>
2025-03-20 14:50:02 +0530

    https://github.com/ishit28/JavaPractice1.git

---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Case Sensitivity: Java is case-sensitive,which means that variable and Variable and VARIABLE are
+        // considered different identifiers
+
+        // Starting character: A  variable  name can begin with a letter (a to z or A to Z
+        // or a dollar sign($). It cannot same with  a digit.
+
+        // Subsequent Characters: After the first character,variable name can contain any combination of letters,
+        // digits (0 to 9), underscores(_) or dollar signs($).
+
+        // No Reserved Keywords: variable name cannot use java reserved keywords (like int, class, static etc.).
+        // These keywords have special meaning in java and are not allowed for variable names.
+
+        // Length: There is no limit to the length of a variable name, but its best to keep it reasonable
+        // for readability purposes.
+
+        // Conventions: While not enforced by java, following naming conventions improves code readability.
+        // Typically variable name start with a lowercase letter and use camelCase for compound names
+        // (e.g., myVariable, studentCount). For constants, use uppercase letters with underscores(e.g., MAX_HEIGHT).
+        int age = 23;
+
+    }
+}
