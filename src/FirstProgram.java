0fac9fa9a24e2fbcf25ca5c4768d6073303b094f
tyagi <tyagiishit91@gmail.com>
2025-03-20 14:50:02 +0530

    https://github.com/ishit28/JavaPractice1.git

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,5 +1,31 @@
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("First Program");
+
+
+        // In Java, there are 3 types of comments – Single Line, Multi-Line, and Documentation comments.
+
+        // Example:
+
+        // 1  This is single line comment
+
+        /* 2 Comment starts
+        ...
+        ...
+        Comment ends*/
+
+        /**  3Comment start
+         *
+         *tags are used in order to specify a parameter
+         *or method or heading
+         *HTML tags can also be used
+         *such as <h1>
+         *
+         *comment ends*/
+
+
+
+
+
     }
 }
---
+++ src/Keywords.java
@@ -0,0 +1,65 @@
+public class Keywords {
+    public static void main(String[] args) {
+
+        // List of Reserved Java Keywords
+
+        // abstract
+        // assert
+        // boolean
+        // break
+        // byte
+        // case
+        // catch
+        // char
+        // class
+        // const
+        // continue
+        // default
+        // double
+        // of
+        // else
+        // enum
+        // extends
+        // false
+        // final
+        // finally
+        // float
+        // for
+        // goto
+        // if
+        // implements
+        // import
+        // instanceof
+        // int
+        // interface
+        // long
+        // native
+        // new
+        // null
+        // package
+        // private
+        // protected
+        // public
+        // return
+        // short
+        // static
+        // strictfp
+        // super
+        // switch
+        // synchronized
+        // this
+        // throw
+        // throws
+        // transient
+        // true
+        // try
+        // void
+        // volatile
+        // while
+
+        // for e.g. for loop
+        for(int i=0;i<10;i++){
+            System.out.println("This is example for reserved keyword");
+        }
+    }
+}
